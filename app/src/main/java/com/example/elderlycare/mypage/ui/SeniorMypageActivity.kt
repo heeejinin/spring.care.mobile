a91b4d834fbee46a1a6fa2bf1118fa6dc3577ed6
WhiteCaCaO <150228758+WhiteCaCaO@users.noreply.github.com>
2024-04-03 16:04:52 +0900

    4.03

--- app/src/main/java/com/example/elderlycare/mypage/ui/SeniorMypageActivity.kt
+++ app/src/main/java/com/example/elderlycare/mypage/ui/SeniorMypageActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.core.view.ViewCompat
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.elderlycare.MainActivity
 import com.example.elderlycare.R
 import com.example.elderlycare.databinding.ActivitySeniorMypageBinding
@@ -30,56 +35,9 @@ class SeniorMypageActivity : AppCompatActivity() {
         binding = ActivitySeniorMypageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //// 네비게이션
-
-        navigationView = findViewById(R.id.nav_view)
-        navViewContainer = findViewById(R.id.nav_view_container)
-
-        // 헤더 레이아웃에서 버튼과 네비게이션 뷰 컨테이너 가져오기
-        val headerLayout = findViewById<RelativeLayout>(R.id.header_layout)
-        val btnMenu = headerLayout.findViewById<ImageButton>(R.id.btnMenu)
-        navViewContainer = headerLayout.findViewById(R.id.nav_view_container)
-
-        // 네비게이션 뷰 초기화
-        navigationView = findViewById(R.id.nav_view)
-
-        // NavigationView 메뉴 아이템 선택 이벤트 처리
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_item1 -> {
-                    // nav_item1 선택 시 처리
-                    startActivity(Intent(this, NavItem1Activity::class.java))
-                }
-                R.id.nav_item2 -> {
-                    // nav_item2 선택 시 처리
-                    startActivity(Intent(this, NavItem2Activity::class.java))
-                }
-                R.id.nav_item3 -> {
-                    // nav_item3 선택 시 처리
-                    startActivity(Intent(this, NavItem3Activity::class.java))
-                }
-                // 다른 메뉴 아이템에 대한 처리
-            }
-            true// true 반환하여 클릭 이벤트 소비
-        }
-
-        // 메뉴 버튼 클릭 이벤트 처리
-        btnMenu.setOnClickListener {
-            toggleNavViewVisibility()
-            // 네비게이션 뷰를 페이지 맨 위로 이동
-            moveNavViewToTop()
-        }
-        // ImageView 클릭 이벤트 처리
-        val imageViewLogo = findViewById<ImageView>(R.id.logo)
-        imageViewLogo.setOnClickListener {
-            // MainActivity로 돌아가는 Intent 생성
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }   //보완필요
-
         val tabLayout = binding.tabs
 
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 val transaction = supportFragmentManager.beginTransaction()
                 Log.d(">>", tab?.text.toString())
@@ -112,27 +70,43 @@ class SeniorMypageActivity : AppCompatActivity() {
         })
         supportFragmentManager.beginTransaction().add(R.id.tabContent, MyInfoFragment()).commit()
 
-
-
-
-
-
-
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val frgMng: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = frgMng.beginTransaction()
+        val myInfoFrg: MyInfoFragment = MyInfoFragment()
+        transaction.add(R.id.tabContent, myInfoFrg)
+        transaction.commit()
+//    twoFrg.setTextV("hello hello")
+//        binding.button.setOnClickListener {
+//            val xx = binding.layoutForTwofrg?.get(R.id.frgtwotxt) as TextView
+//            Log.d(">>", xx.toString())
+////      val frg2txt = frgMng?.findFragmentById(R.id.frgtwotxt) as TextView
+////      frg2txt.text = ">> hello hello"
+//        }
     }
-    private fun toggleNavViewVisibility() {
-        val navViewContainer = findViewById<FrameLayout>(R.id.nav_view_container)
-        if (navViewContainer.visibility == View.VISIBLE) {
-            // 네비게이션 뷰가 보이는 경우, 유지
-            navViewContainer.visibility = View.GONE
-        } else {
-            // 네비게이션 뷰가 숨겨진 경우, 보이게 함
-            navViewContainer.visibility = View.VISIBLE
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            setTitle("Bar")
+            menuInflater.inflate(R.menu.mypage_bar_menu, menu)
+            return super.onCreateOptionsMenu(menu)
         }
-    }
-    private fun moveNavViewToTop() {
-        // 네비게이션 뷰를 페이지 맨 위로 이동
-        ViewCompat.offsetTopAndBottom(navViewContainer, -navViewContainer.top)
-    }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+                R.id.menu1 -> {}
+                R.id.menu2 -> {}
+                R.id.menu3 -> {}
+                else -> {}
+            }
+            return super.onOptionsItemSelected(item)
+        }
+
+
+
+
+
+
+
+
 
 
 
